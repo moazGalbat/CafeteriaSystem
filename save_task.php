89b9d6c8c3877c310da2d0710bd9d92127501250
islamfelfela <islamsalahh@gmail.com>
2020-02-21 09:34:25 +0200

    handling file uploaded

--- save_task.php
+++ save_task.php
@@ -3,7 +3,39 @@
 include('db.php');
 echo "Connected .... ";
 
-move_uploaded_file($_FILES["file"]["tmp_name"],"files/");
+// -------------- check for file ---------------
+
+$filename = $_FILES['file']['name'];
+$filetype = $_FILES['file']['type'];
+$filetmp_name = $_FILES['file']['tmp_name'];
+$filesize = $_FILES['file']['size'];
+$ext = explode(".", $_FILES['file']['name']);
+$fileExt = strtolower(end($ext));
+
+$err = array();
+$extensions = ["png", "jpg", "md"];
+
+var_dump($_FILES);
+die();
+
+
+// Check File Extensions
+if(in_array($fileExt, $extensions) === false){
+    $err[]= "----------Extension is not Allawoed -----";
+}
+
+// Check File Size
+if($filesize > 10000){
+    $err[]= "----------Too Much Size -----";
+}
+
+if(empty($err) == true){
+    move_uploaded_file($filetmp_name,$filename);
+    echo "----------------File Added ----------";
+    var_dump($err);
+}
+
+
 
 
 if (isset($_POST['save_task'])) {
@@ -17,7 +49,6 @@ if (isset($_POST['save_task'])) {
 
 
   $query = "INSERT INTO product(name, price, pic, category_id) VALUES ('$name', '$price, $img_path,$category ')";
-  $query = "INSERT INTO product(name, price, pic, category_id) VALUES ('$name', '$price, $img_path,$category ')";
   $result = mysqli_query($conn, $query);
   if(!$result) {
     die("Query Failed.");
@@ -25,7 +56,7 @@ if (isset($_POST['save_task'])) {
 
   $_SESSION['message'] = 'Task Saved Successfully';
   $_SESSION['message_type'] = 'success';
-  header('Location: addProduct.php');
+  header('Location: index.php');
 
 }
 
