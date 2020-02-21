a0609ee483bf29129c5e4a2f9813269af94dfb52
islamhanafi94 <islamhanafi1994@gmail.com>
2020-02-21 19:08:39 +0200

    styling home page

--- home.php
+++ home.php
@@ -40,40 +40,31 @@ if (!isset($_SESSION['loggedin'])) {
 
     </div>
 
-    <div class="row">
-        <div class="column side" style="background-color:#aaa;">
-            <form id="form" action="insertOrder.php" method="post">
+    <div class="main">
+            <form  class="order-data" id="form" action="insertOrder.php" method="post">
                 <div id="list" class="order">
-                    <!-- <div class="list_element" id="tea_element">
-                    <span>tea</span>
-                    <div>
-                        <button class="minus">-</button>
-                    <input name="quantity" type="text" value="1" data-price="5">
-                    <button class="plus">+</button>
-                    </div>
-                    <span>5</span>
-                    <button class="deleteBtn">X</button>
-                </div> -->
+                    <div>Order</div>
                 </div>
-                <label id="notes" for="notes">Notes:</label>
-                <textarea name="notes" id="notes" rows="4" cols="50">
-                </textarea>
-                <label for="room">Room</label>
-                <select name="room" id="room">
-                    <option value="ttttt">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
+                <div class="notes">
+                    <label id="notes" for="notes">Notes:</label>
+                    <textarea name="notes" id="notes" rows="4" cols="50">
+                    </textarea>
+                </div>
+                <div class="room">
+                    <label for="room">Room</label>
+                    <select name="room" id="room">
+                        <option value="1001">1001</option>
+                        <option value="1002">1002</option>
+                        <option value="1003">1003</option>
+                    </select>
+                </div>
                 <hr>
                 <div id="orderFooter" class="orderFooter">
                     <span id=total></span><br>
                     <button type="submit">Confirm</button>
-                </div>
             </form>
         </div>
-
-        <div class="column middle" style="background-color:#bbb;">
-
+        <div class="product-list-last">
 
             <?php
             include 'config.php';
@@ -85,50 +76,41 @@ if (!isset($_SESSION['loggedin'])) {
             AND order_id=(SELECT order_id FROM orders WHERE user_id=$user_id ORDER by date DESC limit 1 )";
             $stmt = $db->query($query);
             $res = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            echo "<div class='latestOrder'><span>Latest Order</span>";
+            echo "<div class='latest-order'><div class='latestOrder-title'>Latest Order</div>";
             while ($ele = $stmt->fetch()) {
-                echo ("<div>
-                <img width='200px' src={$ele['pic']}  />
+                echo ("<div class='order-item'>
+                <img class='item-img' src={$ele['pic']}  />
                 <p>{$ele['name']}::quantity={$ele['quantity']}</p>
             </div>");
             } 
             echo "</div>";
 
-            //show all products
+            // show all products
             $query = "SELECT product_id,name,price,pic FROM product";
             $stmt = $db->query($query);
 
             $res = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
-            echo "<div class='availableProducts'><span>Available Products</span>";
-
+            echo "<div class='products-list'><div class='produts-list-title'>Available Products</div>";
+            echo "<div class='items-list'>";
             while ($ele = $stmt->fetch()) {
-                echo ("<div>
-                <img class='item' width='200px' data-price={$ele['price']} data-name={$ele['name']} data-id={$ele['product_id']} src={$ele['pic']}  />
-                <p>{$ele['price']} EPG</p>
+                echo ("<div class=item>
+                <img class='item-img'  data-price={$ele['price']} data-name={$ele['name']} data-id={$ele['product_id']} src={$ele['pic']}  />
+                <div>{$ele['name']} </div>
+                <div>{$ele['price']} L.E</div>
+
             </div>");
             }
             echo "</div>";
+            echo "</div>";
             $db = null;
             ?>
-            <!-- <div>
-                <img class="item" data-price="7" data-name="tea" id="tea" src="images/image.jpg" alt="" />
-                <p>price</p>
-            </div>
-            <div>
-                <img class="item" data-price="7" data-name="coffe" id="coffe" src="images/image.jpg" alt="" />
-                <p>price</p>
-            </div>
-            <div>
-                <img class="item" data-price="7" data-name="cola" id="cola" src="images/image.jpg" alt="" />
-                <p>price</p>
-            </div> -->
         </div>
     </div>
-
-    <div class="footer">
+    <!-- <div class="footer">
         <p>Footer</p>
-    </div>
+    </div> -->
+
 
     <script src="home.js"></script>
 </body>
