03507c55064dc016a7ba2c6599129a675b541be1
shimaaibrahim1 <ir.shaimaa@gmail.com>
2020-02-21 14:14:18 +0200

    add user page

---
+++ addUser.php
@@ -0,0 +1,98 @@
+<?php
+$response="nothing";
+$response=$_GET['user'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="addUser.css" rel="stylesheet" type="text/css">
+    <title>cafeteria/addUser</title>
+</head>
+<body onload="dbreaction()">
+    <div id="main-container">
+        <div id="card">
+            <div id="header">
+                <p><strong>ADD USER</strong> </p>
+            </div>
+            <div id="form-container">
+                <table>
+                    <form action="addUserdb.php" method="POST" id="form" enctype="multipart/form-data">
+                        <tr>
+                            <td><label>User Name</label></td>
+                            <td><input class="input" type="text" name="username" pattern="[a-zA-Z_]+[a-zA-Z0-9]{2,10}" title="Name should start with letter or underscore, 3 characters at least 'No special characters are allowed'" required></td>
+                        </tr>
+                        <tr>
+                            <td><label>Email</label></td>
+                            <td><input class="input" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></td>
+                        </tr>
+                        <tr>
+                        <td><label>Password</label></td>
+                        <td><input class="input" type="password" name="password" pattern="[a-zA-Z0-9._%+-]{3,}" title="password should not be less than 3 characters or contain a special character" required></td>
+                        </tr>
+                    <tr>
+                        <td><label>Confirm password</label></td>
+                        <td><input class="input" type="password" name="confirm" required></td>
+                        </tr>
+                        <tr>
+                            <td><label>Room</label></td>
+                            <td><input class="input" type="text" name="room" pattern="[0-9]{0,5}"></td>
+                        </tr>
+                        <tr>
+                            <td><label>Ext.</label></td>
+                            <td><input class="input" type="tel" name="ext" pattern="01[0-9]{9}"></td>
+                        </tr>
+                        <tr>
+                            <td><label>Profile Picture</label></td>
+                            <td><input type="file" name="image" id="img" accept=".png, .jpeg, .jpg"></td>
+                        </tr>
+                        <tr>
+                        <td><input type="hidden" name="hidden_id" value=""></td>
+                        </tr>
+                    </form>
+                    </table>
+
+            </div>
+            <div id="footer">
+                <input type="submit" form="form" id="save" value="save">
+                <input type="reset" form="form" id="reset">
+            </div>
+    </div>
+    <div id="notification"></div>
+</div>
+<script>
+let password = document.querySelector("[name=password]");
+let confirm = document.querySelector("[name=confirm]");
+function confimation(){
+  if(password.value != confirm.value) {
+    confirm.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm.setCustomValidity('');
+  }
+password.addEventListener("change",confimation);
+confirm.addEventListener("change",confimation);   
+}
+
+
+let response = <?php echo json_encode($response)?>;
+console.log(response)
+let notification = document.getElementById("notification");
+function dbreaction(){
+    if(response == "accepted"){
+        notification.innerHTML="<p class='note'>User added successfully</p>";
+        notification.style.display='block';
+        notification.style.animation='notify 5s forwards';
+    
+}else if(response == "notAccepted"){
+    notification.innerHTML="<p class='note'>User already exists</p>";
+        notification.style.display='block';
+        notification.style.animation='notify 2s forwards';
+        
+}
+}
+
+</script>
+</body>
+</html>
