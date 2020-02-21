520c694614723a4fba6589ff6273b7702ec318d9
ismailkhadr1995 <ismailkhadr80@yahoo.com>
2020-02-21 02:51:35 +0200

    adding validation and rename table order to orders

--- showOrder.php
+++ showOrder.php
@@ -26,62 +26,65 @@ else{
 
 if(isset ($_POST['from']) && ($_POST['to']))
 { 
-    
-    
-
     if(isset($_POST['username']))
        {     $user_name=$_POST['username'];
               //echo $user_name;
             echo "<table><tr><th>Name</th><th>Total amount</th></tr>";
             $query="SELECT SUM(price*quantity)as total ,username
-            from orderT ,order_product , product ,user
-            where orderT.order_id=order_product.order_id AND 
+            from orders ,order_product , product ,user
+            where orders.order_id=order_product.order_id AND 
             product.product_id=order_product.product_id 
-            and user.user_id=orderT.user_id  and date between '$from' AND '$to' 
+            and user.user_id=orders.user_id  and date between '$from' AND '$to' 
+            and username='".$user_name."'
             GROUP by date";
             $result = mysqli_query($conn, $query);
-            
-            while($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td>". $row["total"]."</td><td>". $row["username"]. "</td></tr>";
-            }
-              echo "</table>";
-            
-            
-            
-       }
+            $num_rows = mysqli_num_rows($result);
+            if($num_rows>0){
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr><td>". $row["total"]."</td><td>". $row["username"]. "</td></tr>";
+                }
+                echo "</table>";
+              }
+            else
+             {
+                 echo "0 rows !";
+             }
+        }
        
        else 
        {
             echo "<table><tr><th>totalAmount </th><th>orderDate</th></tr>";
             $query="SELECT SUM(price*quantity)as total,date as orderDate
-            from orderT ,order_product ,product 
-            where orderT.order_id=order_product.order_id AND 
+            from orders ,order_product ,product 
+            where orders.order_id=order_product.order_id AND 
             product.product_id=order_product.product_id 
             and date between '$from' AND '$to'
             GROUP by date";
             $result = mysqli_query($conn, $query);
-            while($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td> " . $row["total"]." </td> <td>". $row["orderDate"]. "</td></tr>";
+            $num_rows = mysqli_num_rows($result);
+            if($num_rows>0)
+             {
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr><td> " . $row["total"]." </td> <td>". $row["orderDate"]. "</td></tr>";
+                } 
+                echo "</table>";
+            }
+            else 
+                {
+                    echo "0 rows !";
+
+                }
+
+
         }
-
-
-       }
      
 }
 else 
 {
-     
-    echo "you didn't selet any date or user :";
+   echo "you didn't selet any date or user :";
     header('location:http://localhost/CafeteriaSystem/php_project/select.php');
-
-
-
 }
 
 }
-
-
-
-
 
 ?>
