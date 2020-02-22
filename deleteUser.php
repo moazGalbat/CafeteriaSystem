2ebeb3d0297be1aa119f6311b13ec7bbb3b1b406
root <root@192.168.1.8>
2020-02-22 20:12:15 +0200

    modification of delete user and update orderPage

---
+++ deleteUser.php
@@ -0,0 +1,18 @@
+<?php
+require 'config.php';
+$deleteId="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (empty($_POST["userid"])) {
+  $deleteId="";
+}else{
+  $deleteId=$_POST['userid'];
+}
+}
+
+#delete from db
+$sql= "DELETE FROM user where user_id=?";
+$stmt=$conn->prepare($sql);
+$stmt->execute([$deleteId]);
+
+
+?>
